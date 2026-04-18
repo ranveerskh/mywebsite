9966fae691d98d584de4dc16a03487716d3dc5ac
ranveerskh <ranveerskh@gmail.com>
2026-04-18 15:45:23 -0400

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDr6kRdTjdLEoq0398g1bN4D4a9Fm-CMfo",
+  authDomain: "bareworld.firebaseapp.com",
+  projectId: "bareworld",
+  storageBucket: "bareworld.firebasestorage.app",
+  messagingSenderId: "1010059894849",
+  appId: "1:1010059894849:web:58976414fe92490d5766c0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
